d9a1d811d8905e2def1e19b031f92b0e3cda87d9
Sanishmdhr <sanish.manandhar20@gmail.com>
2020-09-17 20:58:22 +0545

    Header UI Done

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDVeuK_OJkY2fVZtBti3MxLy8piD8sb6Xk",
+  authDomain: "facebook-clone-8c877.firebaseapp.com",
+  databaseURL: "https://facebook-clone-8c877.firebaseio.com",
+  projectId: "facebook-clone-8c877",
+  storageBucket: "facebook-clone-8c877.appspot.com",
+  messagingSenderId: "229811546536",
+  appId: "1:229811546536:web:3851342d475fd19d7f959d"
+};
