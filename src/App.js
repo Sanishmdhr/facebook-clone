5f94eb643a78f989ab8841c6c548de4f3caa5328
Sanishmdhr <sanish.manandhar20@gmail.com>
2020-09-19 11:26:25 +0545

    Page embed Section

--- src/App.js
+++ src/App.js
@@ -3,24 +3,18 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Widgets from './Widgets';
 
 
 function App() {
   return (
     <div className="app">
-
         <Header/>
-        
         <div className="app__body">
-
           <Sidebar/>
           <Feed/>
-
+          <Widgets/>
         </div>
-
-          {/* Feed */}
-          {/* Widgets */}
-
     </div>
   );
 }
